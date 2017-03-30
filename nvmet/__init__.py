JunxiongGuan/nvmetcli

commit a21ebd54f0cb58e62a3d9d751d486e241edf9e4b

nvmetcli: Fix nvmet import
We should just need to import nvmet, which will run __init__.py and import
just the public things from nvmet/nvme.py.

Signed-off-by: Andy Grover <agrover@redhat.com>
Signed-off-by: Christoph Hellwig <hch@lst.de>

nvmetcli
@@ -23,7 +23,7 @@ from __future__ import print_function
 import os
 import sys
 import configshell_fb as configshell
-import nvmet.nvme as nvme
+import nvmet as nvme
 
 
 class UINode(configshell.node.ConfigNode):

commit 65bf3de35b85b6d27498c41cc070970ad50039ec

nvmetcli: fix Referral operation in shell
It's not possible to create or list a Referral under Port entity,
because Referral class is not imported and thus it fails:

/ports/1/referrals> ls

returning "'module' object has no attribute 'Referral'".

This is caused by changes introduced in:
a21ebd54f0 ("nvmetcli: Fix nvmet import").

This patch also fixes the attribute type helper expected for Referral's
portid attribute. It was 'int', causing the system to show error
messages like:

"'UIReferralNode' object has no attribute 'ui_type_int'".
The correct type is 'number'.

Reviewed-by: Guilherme G. Piccoli<gpiccoli@linux.vnet.ibm.com>
Reviewed-by: Sagi Grimberg <sagi@grimberg.me>
Signed-off-by: Mauro S. M. Rodrigues <maurosr@linux.vnet.ibm.com>
Signed-off-by: Christoph Hellwig <hch@lst.de>

nvmet/__init__.py
@@ -1,1 +1,1 @@
-from .nvme import Root, Subsystem, Namespace, Port, Host
+from .nvme import Root, Subsystem, Namespace, Port, Host, Referral

nvmetcli
@@ -460,7 +460,7 @@ class UIReferralNode(UINode):
         'traddr': ('string', 'Transport Address (e.g. IP Address)'),
         'trsvcid': ('string', 'Transport Service ID (e.g. IP Port)'),
         'trtype': ('string', 'Transport Type (e.g. rdma or loop)'),
-        'portid': ('int', 'Port identifier'),
+        'portid': ('number', 'Port identifier'),
     }
 
     def __init__(self, parent, cfnode):